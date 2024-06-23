f7b64e820aef8801497df5f36a97d1382073f8ac
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-06-24 01:17:40 +0200

    Add hash

--- pkgs/by-name/esp32-open-mac/package.nix
+++ pkgs/by-name/esp32-open-mac/package.nix
@@ -11,8 +11,8 @@ stdenv.mkDerivation {
     owner = "esp32-open-mac";
     repo = "esp32-open-mac";
     rev = "96eb701991b4c23fcad2332934a6f9519e76fefa";
-    hash = "";
+    hash = "sha256-TBgCUIsI4efrpWnucYL2A2Zj3bt52Sx0IErG5Stcdx0=";
   };
 
-  nativeBuildInputs = [cmake ];
+  nativeBuildInputs = [ cmake ];
 }
